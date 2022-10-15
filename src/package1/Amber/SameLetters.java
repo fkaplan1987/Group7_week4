01c06dc391b01f85c8989565f0349005c85ffe2c
Amber Porras <amberporras@gmail.com>
2022-10-15 11:08:34 -0400

    Created two classes

---
+++ src/package1/Amber/SameLetters.java
@@ -0,0 +1,24 @@
+package package1.Amber;
+
+import java.util.Arrays;
+
+public class SameLetters {
+    public static boolean same(String a, String b) {
+        char[] ch1 = a.toCharArray();
+        char[] ch2 = b.toCharArray();
+
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        String a1 = "", a2 = "";
+
+        for (char each : ch1) {
+            a1 += each;
+        }
+
+        for (char each : ch2) {
+            a2 += each;
+        }
+
+        return a1.equals(a2);
+    }
+}
