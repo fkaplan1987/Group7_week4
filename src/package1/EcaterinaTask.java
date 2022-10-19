b03a6a0aaf45300fd587321f6d4a8396f64829c4
Ecaterina <ecaterinacoseru@gmail.com>
2022-10-19 11:36:37 -0400

    EcaterinaTask 1 added

--- src/package1/EcaterinaTask.java
+++ src/package1/EcaterinaTask.java
@@ -1,4 +1,36 @@
 package package1;
 
 public class EcaterinaTask {
+
+/*
+    String -- Frequency of Characters
+    Write a return method that can find the frequency of characters
+    Ex: FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
+     */
+
+    public static String frequencyOfChar(String str) {
+
+
+        String result = "";
+        String[] temp = str.split("");
+
+        for (int i = 0; i < temp.length; i++) {
+            int count = 0;
+            String s = "";
+            for (int j = 0; j < temp.length; j++) {
+                if (temp[i].equals(temp[j])) {
+                    count++;
+                }
+            }
+            s += temp[i] + count;
+            if(!result.contains(s))
+                result += s;
+        }
+        return result;
+    }
+
+    public static void main (String[]args){
+        System.out.println(frequencyOfChar("AAABBCDD"));
+    }
 }
+
